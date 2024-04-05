4134eb10566c592289f3f94462a321b59cad212b
RobP910 <robb910@hotmail.com>
2024-04-04 18:19:03 -0600

    Troubleshooting

--- customer_banking.py
+++ customer_banking.py
@@ -1,6 +1,6 @@
 # Import the create_cd_account and create_savings_account functions
-import utilities.create_cd_account
-import utilities.create_savings_account
+from cd_account import create_cd_account
+from savings_account import create_savings_account
 
 # Define the main function
 def main():
@@ -8,10 +8,16 @@ def main():
     and the length of months to determine the interest gained.
     It displays the interest earned on the savings and CD accounts and updates the balances.
     """
+    # Creating an instance of MyClass
+    obj = Account("Alice")
+
+    # Using the greet method of MyClass
+    obj.greet()
+
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
     savings_balance = input("\nWhat is the starting balance? ")
     savings_interest = input("\nWhat is the interest rate? ")
-    savings_maturity = input("\nHow many months are your terms? ")
+    savings_maturity = input("\nHow many months before maturity? ")
 
     # Call the create_savings_account function and pass the variables from the user.
     updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
@@ -19,7 +25,7 @@ def main():
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
     for i in savings_maturity:
         print(" MONTH | Interest Earned | Balance")
-        print(f"{i} :<7 | {interest_earned} | {updated_savings_balance}")
+        print(f" {i} :<7 | {interest_earned} | {updated_savings_balance}")
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     cd_balance = input("\nWhat is the cd contribution amount? ")
